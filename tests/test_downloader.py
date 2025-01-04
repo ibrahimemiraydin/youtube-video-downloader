a8112783ffc9dec532d668d83f105392cc848e7d
Emir Aydın <84669393+ibrahimemiraydin@users.noreply.github.com>
2025-01-04 12:59:34 +0300

    Delete test_downloader.py

--- tests/test_downloader.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from core.downloader import download_video
-
-def test_download_video():
-    url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-    try:
-        download_video(url)
-    except Exception:
-        pytest.fail("Download failed")
